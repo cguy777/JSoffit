45ae5281e112245a5801caf1b4c5845782841e9c
cguy777 <noahmclean@sbcglobal.net>
2023-04-28 23:44:54 -0400

    Added SoffitException

---
+++ src/fibrous/soffit/SoffitException.java
@@ -0,0 +1,11 @@
+package fibrous.soffit;
+
+public class SoffitException extends RuntimeException {
+	SoffitException() {
+		super();
+	}
+	
+	SoffitException(String message) {
+		super(message);
+	}
+}
