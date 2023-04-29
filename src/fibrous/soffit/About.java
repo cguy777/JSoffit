381b3db72d69ed089e682309cbd0f8f10d2e5ffe
cguy777 <noahmclean@sbcglobal.net>
2023-04-29 15:10:05 -0400

    Update About.java

--- src/fibrous/soffit/About.java
+++ src/fibrous/soffit/About.java
@@ -2,7 +2,7 @@ package fibrous.soffit;
 
 public class About {
 	
-	private static final String VERSION = "1.1.1";
+	private static final String VERSION = "0.1.0";
 	
 	/**
 	 * Returns the version of SOFFIT that this library is compliant with.
