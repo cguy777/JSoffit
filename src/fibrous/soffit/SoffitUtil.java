f4b9f9c5144b1e2405b339aa919daf9a89bd2f9a
cguy777 <noahmclean@sbcglobal.net>
2024-09-15 19:25:28 -0400

    Removed unused imports

--- src/fibrous/soffit/SoffitUtil.java
+++ src/fibrous/soffit/SoffitUtil.java
@@ -38,8 +38,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
 import java.util.Stack;
 
 //*********************************************************
